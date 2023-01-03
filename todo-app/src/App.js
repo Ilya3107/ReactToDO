4e053755bfd33cb60213fd5613d5ba1598dc739e
”Ilya <poryadinilya3107@gmail.com>
2023-01-03 15:24:35 +0300

    Add first component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import React from "react";
+import listSvg from "./assets/img/list.svg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="todo">
+      <div className="todo__sidebar">
+        <ul className="todo__list">
+          <li>
+            <i>
+              <img src={listSvg} alt="list Icon" />
+            </i>
+            <span>Все задачи</span>
+          </li>
+        </ul>
+      </div>
+      <div className="todo__tasks"></div>
+    </div>
+  );
 }
 
 export default App;
